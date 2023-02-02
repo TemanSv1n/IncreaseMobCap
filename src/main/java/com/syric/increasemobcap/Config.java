3f6a61a05dc87fa43f8c3311e78162965386c49d
Syric <76715986+Syrikal@users.noreply.github.com>
2023-02-01 19:53:41 -0500

    Started 1.18.2 port

--- src/main/java/com/syric/increasemobcap/Config.java
+++ src/main/java/com/syric/increasemobcap/Config.java
@@ -20,6 +20,8 @@ public class Config {
 	public static ForgeConfigSpec.ConfigValue<Integer> AMBIENT_CAP;
 	public static ForgeConfigSpec.ConfigValue<Integer> WATER_CREATURE_CAP;
 	public static ForgeConfigSpec.ConfigValue<Integer> WATER_AMBIENT_CAP;
+	public static ForgeConfigSpec.ConfigValue<Integer> UNDERGROUND_WATER_CAP;
+	public static ForgeConfigSpec.ConfigValue<Integer> AXOLOTL_CAP;
 	
 	static {
 		COMMON_BUILDER.push("Mob Caps");
@@ -28,12 +30,16 @@ public class Config {
 				"Controls the maximum number of zombies, creepers, etc. Default: 70").define("monster", MONSTER.getMaxInstancesPerChunk());
 		CREATURE_CAP = COMMON_BUILDER.comment("Mob cap for creatures. " +
 				"Controls the maximum number of miscellaneous creatures. Default: 10").define("creature", CREATURE.getMaxInstancesPerChunk());
-		AMBIENT_CAP = COMMON_BUILDER.comment("Mob cap for ambient. " +
+		AMBIENT_CAP = COMMON_BUILDER.comment("Mob cap for ambient creatures. " +
 				"Controls the maximum number of bats. Default: 15").define("ambient", AMBIENT.getMaxInstancesPerChunk());
 		WATER_CREATURE_CAP = COMMON_BUILDER.comment("Mob cap for water creatures. " +
 				"Controls the maximum number of squid, dolphins, etc. Default: 5").define("water_creature", WATER_CREATURE.getMaxInstancesPerChunk());
 		WATER_AMBIENT_CAP = COMMON_BUILDER.comment("Mob cap for ambient water creatures. " +
 				"Controls the maximum number of fish. Default: 20").define("water_ambient", WATER_AMBIENT.getMaxInstancesPerChunk());
+		UNDERGROUND_WATER_CAP = COMMON_BUILDER.comment("Mob cap for underground water creatures. " +
+				"Controls the maximum number of glowsquid. Default: 5").define("underground_water_creature", UNDERGROUND_WATER_CREATURE.getMaxInstancesPerChunk());
+		AXOLOTL_CAP = COMMON_BUILDER.comment("Mob cap for axolotls. " +
+				"Controls the maximum number of axolotls. Default: 5").define("axolotl", AXOLOTLS.getMaxInstancesPerChunk());
 		COMMON_BUILDER.pop();
 
 		COMMON_SPEC = COMMON_BUILDER.build();
--- src/main/java/com/syric/increasemobcap/IncreaseMobCap.java
+++ src/main/java/com/syric/increasemobcap/IncreaseMobCap.java
@@ -1,15 +1,15 @@
 package com.syric.increasemobcap;
 
 import net.minecraft.world.entity.MobCategory;
-import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
+import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
+import net.minecraftforge.network.NetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -31,7 +31,9 @@ public class IncreaseMobCap {
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 		
 		//Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
-		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+//		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> Pair.of(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
+//		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, ()->new IExtensionPoint.DisplayTest(()-> NetworkConstants.IGNORESERVERONLY, (s,b)->true));
 	}
 	
 	private void setup(final FMLCommonSetupEvent event) {
@@ -41,7 +43,7 @@ public class IncreaseMobCap {
 //		}
 		
 		try {
-			Field mobCap = ObfuscationReflectionHelper.findField(MobCategory.class, "field_75606_e");
+			Field mobCap = ObfuscationReflectionHelper.findField(MobCategory.class, "");
 			mobCap.setAccessible(true);
 			
 			Field modifierField = Field.class.getDeclaredField("modifiers");
